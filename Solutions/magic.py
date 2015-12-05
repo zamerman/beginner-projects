d87df38c75e6d67bad26033b50be836245643a57
jorge <alfredmuffin@gmail.com>
2015-12-04 21:26:16 -0500

    Move welcome message

--- Solutions/magic.py
+++ Solutions/magic.py
@@ -1,4 +1,4 @@
-import time, sys
+from time import sleep
 from random import randint
 
 # list of responses
@@ -6,19 +6,14 @@ responses = ["Yes", "No", "Probably", "Maybe", "I don't think so", "Perhaps",
              "It's possible", "Yes, I think so", "That seems likely", "Sure",
              "That's not possible", "I would hope so", "Sounds like it could be true", "You shoudn't do that", "But why?", "Dreadfully tinny word, that is", "The cake is a lie!", "A man chooses. A slave obeys.", "Did you set it to Wumbo?", "42"]
 
-# welcome message
-print("\nWelcome to the Magic 8 ball!")
-
-# main function
 def Magic():
     question = input("Ask me a question: ")
     print("\nThinking ...")
-    time.sleep(1.5)
+    sleep(1)
 
     print(responses[randint(0, 20)], "\n")
     Loop()
 
-# main loop
 def Loop():
     play = input("Would you like to play again? (Y/N) ")
 
@@ -31,5 +26,8 @@ def Loop():
     else:
         print("I didn't quite catch that.")
         Loop()
+
+# welcome message
+print("\nWelcome to the Magic 8 ball!")
 
 Magic()
