6f395e0c0758981be456b8735fb6814d3e95fbc2
jorge <alfredmuffin@gmail.com>
2015-12-04 20:43:39 -0500

    Change main loops into functions

--- Solutions/magic.py
+++ Solutions/magic.py
@@ -1,26 +1,35 @@
+import time, sys
 from random import randint
 
+# list of responses
 responses = ["Yes", "No", "Probably", "Maybe", "I don't think so", "Perhaps",
-             "It's possible", "Yes, I think so"]
+             "It's possible", "Yes, I think so", "That seems likely", "Sure",
+             "That's not possible", "I would hope so", "Sounds like it could be true", "You shoudn't do that", "But why?", "Dreadfully tinny word, that is", "The cake is a lie!", "A man chooses. A slave obeys.", "Did you set it to Wumbo?", "42"]
 
-print("Welcome to the Magic 8 ball!")
+# welcome message
+print("\nWelcome to the Magic 8 ball!")
 
-while True:
+# main function
+def Magic():
     question = input("Ask me a question: ")
-    print responses[randint(0, 7)]
+    print("\nThinking ...")
+    time.sleep(1.5)
 
-    while True:
-        play = input("Would you like to play again? (Y/N) ")
-        print ""
+    print(responses[randint(0, 20)], "\n")
+    Loop()
 
-        if play == "Y" or play == "y":
-            continue
+# main loop
+def Loop():
+    play = input("Would you like to play again? (Y/N) ")
 
-        elif play == "N" or play == "n":
-            print("Goodbye!")
-            break
+    if play == "Y" or play == "y":
+        Magic()
 
-        else:
-            print("I didn't quite catch that.")
+    elif play == "N" or play == "n":
+        print("Goodbye!")
 
-    break
+    else:
+        print("I didn't quite catch that.")
+        Loop()
+
+Magic()
