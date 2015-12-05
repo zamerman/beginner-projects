398e8ed80ee3a8d6cc9fc441d46818088c361af9
jorge <alfredmuffin@gmail.com>
2015-12-04 19:58:56 -0500

    Fix structure of printed text

--- Solutions/bottles.py
+++ Solutions/bottles.py
@@ -4,12 +4,10 @@ count = count[::-1]
 for num in count:
     if num != 1:
         print("{0} bottles of beer on the wall, {0} bottles of beer.".format(num))
-        print("Take one down and pass it around"),
-        print("{0} bottles of beer on the wall!\n".format(num - 1))
+        print("Take one down and pass it around, {0} bottles of beer on the wall!\n".format(num - 1))
     if num == 1:
         print("{0} bottle of beer on the wall, {0} bottle of beer.".format(num))
-        print("Take one down and pass it around"),
-        print("No more bottles of beer on the wall!\n")
+        print("Take one down and pass it around, no more bottles of beer on the wall!\n")
 
 print("No more bottles of beer on the wall, no more bottles of beer.")
 print("Go to the store and buy some more, 99 bottles of beer on the wall!\n")
