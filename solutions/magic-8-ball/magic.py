621e59025d957c0fac534622765205a99303e0ed
Jorge Gonzalez <jrgqhd@gmail.com>
2015-12-21 15:21:07 -0500

    Update magic.py

--- solutions/magic-8-ball/magic.py
+++ solutions/magic-8-ball/magic.py
@@ -8,30 +8,31 @@ from random import randint
 # list of responses
 responses = ["Yes", "No", "Probably", "Maybe", "I don't think so", "Perhaps",
              "It's possible", "Yes, I think so", "That seems likely", "Sure",
-             "That's not possible", "I would hope so", "Sounds like it could be true", "You shoudn't do that", "But why?", "Dreadfully tinny word, that is", "The cake is a lie!", "A man chooses. A slave obeys.", "Did you set it to Wumbo?", "42"]
+             "That's not possible", "I would hope so", "Sounds like it could be true", 
+             "You shoudn't do that", "But why?", "Dreadfully tinny word, that is", 
+             "The cake is a lie!", "A man chooses. A slave obeys.", "Did you set it to Wumbo?", "42"]
 
-def Magic():
+def magic():
     question = input("Ask me a question: ")
     print("\nThinking ...")
     sleep(1)
 
     print(responses[randint(0, 20)], "\n")
-    Loop()
+    loop()
 
-def Loop():
-    play = input("Would you like to play again? (Y/N) ")
+def loop():
+    play = input("Would you like to play again? (Y/N) ").lower()
 
-    if play == "Y" or play == "y":
+    if play == "y":
         Magic()
 
-    elif play == "N" or play == "n":
+    elif play == "n":
         print("Goodbye!")
 
     else:
         print("I didn't quite catch that.")
-        Loop()
+        loop()
 
 # welcome message
 print("\nWelcome to the Magic 8 ball!")
-
-Magic()
+magic()
