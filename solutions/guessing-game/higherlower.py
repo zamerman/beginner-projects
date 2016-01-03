33adb11f2e567d7e46eea0ccac027add241eecdc
jorge <jrgqhd@gmail.com>
2016-01-03 17:49:32 -0500

    Minor changes

--- solutions/guessing-game/higherlower.py
+++ solutions/guessing-game/higherlower.py
@@ -3,29 +3,34 @@
 # Written by rsz
 
 import random
-choice = 'yes'
-print("Welcome")
-print("Try to guess the number")
+
 def game():
     guesses = 0
-    # assigns a random number 
-    number = random.randint(1,100) 
+    # assign a random number
+    number = random.randint(1, 100)
+
+    # main loop
     while True:
-        n = int(input("Guess the number "))
+        n = int(input("Enter your guess: "))
         if n == number:
             guesses += 1
             print("You guessed correctly, the number was ", n)
             print("You took ", guesses," guesses ")
-            choice = input('Play again? ').lower()
-            if choice == 'yes' or choice == 'y':
+
+            choice = input("Play again? (y/n)").lower()
+            if choice == "yes" or choice == "y":
                 game()
             else:
-                    break
-            
+                break
+
         elif n < number:
-            print("Try higher Numbers\n ")
-            guesses += 1
+            print("Try higher Numbers\n")
         else:
-            print("Try lower Numbers \n")
-            guesses += 1
+            print("Try lower Numbers\n")
+
+        guesses += 1
+
+print("Welcome")
+print("Try to guess the number")
+
 game()
